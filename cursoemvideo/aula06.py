be9937169aa4f3ecc152d5398eca5c5dc612b9ba
vitor-fv <technologyvitor@gmail.com>
2024-03-27 15:05:08 -0300

    Initial Commit

--- cursoemvideo/aula06.py
+++ cursoemvideo/aula06.py
@@ -3,8 +3,4 @@ n2 = int(input('Digite outro: '))
 s = n1 + n2
 print('A soma vale', s)
 
-n1 = int(input('Digite um valor: '))
-n2 = int(input('Digite outro: '))
-s = n1 + n2
-print('A soma vale', s)
 
